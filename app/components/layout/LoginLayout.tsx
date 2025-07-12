846c39d4dc1ac333f3b3b43340054bff868a5e5d
terabayashik <terabayashik@mediaedge.co.jp>
2025-07-12 23:06:39 +0900

    Rename folder

---
+++ app/components/layout/LoginLayout.tsx
@@ -0,0 +1,23 @@
+import { Anchor, Group, Title } from "@mantine/core";
+import { Link } from "react-router";
+import { ThemeToggle } from "../ThemeToggle";
+
+interface LoginLayoutProps {
+  children: React.ReactNode;
+}
+
+export const LoginLayout = ({ children }: LoginLayoutProps) => {
+  return (
+    <>
+      <Group justify="space-between" p="md">
+        <Anchor component={Link} to="/" td="none">
+          <Title order={3} c="blue">
+            もっく！つながるサイネージ
+          </Title>
+        </Anchor>
+        <ThemeToggle />
+      </Group>
+      {children}
+    </>
+  );
+};
